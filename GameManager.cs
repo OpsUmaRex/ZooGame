ac6d31077f49a710885a2e986ef503884433a5d7
Adam Cholewinski <halfcholi@gmail.com>
2017-04-03 10:24:44 -0500

    Game Manager & Zoo Scripts

---
+++ GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    [SerializeField]
+    Text VisitorText;
+    [SerializeField]
+    Text Revenue;
+
+    Zoo zoo;
+    public float timer = 10;
+	// Use this for initialization
+	void Start () {
+        zoo = GameObject.FindObjectOfType<Zoo>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            zoo.SetZooState();
+            zoo.CheckZooState();
+            zoo.VisNum -= zoo.visitorDecline;
+            timer = 10;
+        }
+        VisitorText.text = "Vistiors: " + zoo.VisNum;
+        Revenue.text = "Current Revenue: " + zoo.CurrentRevenue;
+	}
+}
